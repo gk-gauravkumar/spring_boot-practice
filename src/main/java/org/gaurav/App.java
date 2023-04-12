06f9d5be5cc1fa8e01b7d3592ccf575787dfaf4d
gk-gauravkumar <gaurav@Gauravs-Mac-mini.local>
2023-04-12 10:51:11 +0530

    first commit

---
+++ src/main/java/org/gaurav/App.java
@@ -0,0 +1,13 @@
+package org.gaurav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
