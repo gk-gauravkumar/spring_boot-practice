9a9902a76b16a4c32377dfe9ecb86f6931326185
gk-gauravkumar <gaurav@Gauravs-Mac-mini.local>
2023-04-12 10:52:54 +0530

    SpringBoot start Dependencies injection

--- src/main/java/org/gaurav/App.java
+++ src/main/java/org/gaurav/App.java
@@ -1,13 +1,16 @@
 package org.gaurav;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        car car = new car();            car.test();
+//        bike bike = new bike();         bike.test();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Vehicle obj =  (Vehicle) context.getBean("vehicle");
+        obj.test();
     }
 }
---
+++ src/main/java/org/gaurav/Car.java
@@ -0,0 +1,8 @@
+package org.gaurav;
+
+public class Car implements Vehicle{
+    @Override
+    public void test() {
+        System.out.println("car is working tested ok");
+    }
+}
